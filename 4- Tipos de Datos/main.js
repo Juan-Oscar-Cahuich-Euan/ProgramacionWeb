6d5b43994cc88ba48757941a646e9f9467b94f8d
Juan-Oscar-Cahuich-Euan <13470522@itcampeche.edu.mx>
2026-04-05 19:11:16 -0600

    programa 3 y 4 terminado

---
+++ 4- Tipos de Datos/main.js
@@ -0,0 +1,14 @@
+
+let frase = "Hola soy JUAN";        // String O cadena de texto
+let anio = 2026;                    // Numero entero / int
+let interes = 99.5;                  // Numero decimal / float / Double
+let MayorEdad = true;               // Booleano / boolean
+let vacia;                          // Undefined
+let nula = null;                    // Null
+
+let frutas = ["Manzana", "Banana", "Naranja"]; // Array o Arreglo
+let heroe = { nombre: "Superman", poder: "Vuelo" }; // Objeto
+
+console.log(frase + interes + MayorEdad);
+
+
