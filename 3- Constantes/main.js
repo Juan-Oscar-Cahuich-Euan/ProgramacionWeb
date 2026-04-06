3a1c311e5fc6937d828d0c4e45819af3ea572abe
Juan-Oscar-Cahuich-Euan <13470522@itcampeche.edu.mx>
2026-04-05 18:39:54 -0600

    programa 3 y 4

---
+++ 3- Constantes/main.js
@@ -0,0 +1,11 @@
+// Constantes 
+// Variable cuyo contenido nunca jamas  de los Jamases cambia
+
+let nombre = "Juan";
+let web = "www.juan.com";
+const DNI = "13470522";
+
+nombre = "Oscar";
+// no se puede hacer DNI = "123445678Ñ"
+
+console.log(nombre + DNI);
